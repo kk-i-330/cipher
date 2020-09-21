caa9dcdffbdf24e1c119dfdda6dcdd29ffb3008a
kk-i-330 <71608012+kk-i-330@users.noreply.github.com>
2020-09-21 11:46:51 +0500

    Расшифровка и шифровка сообщения(доработанный вид)
    Добавление букв в верхнем регистре

--- cipher.py
+++ cipher.py
@@ -1,5 +1,6 @@
 lowercase = ['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
-
+uppercase = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+ 
  
 def encrypt(msg, shift=1):
     cipher = ""
@@ -7,6 +8,9 @@ def encrypt(msg, shift=1):
         if x in lowercase:
             ind = lowercase.index(x)
             cipher += lowercase[ind+shift]
+        elif x in uppercase:
+            ind = uppercase.index(x)
+            cipher += uppercase[ind+shift]
         else:
             cipher += x
     return cipher
@@ -17,9 +21,14 @@ def decrypt(msg, shift=1):
         if x in lowercase:
             ind = lowercase.index(x)
             cipher += lowercase[ind-shift]
+        elif x in uppercase:
+            ind = uppercase.index(x)
+            cipher += uppercase[ind-shift]
         else:
             cipher += x
     return cipher
 
 print(encrypt(input('Введите сообщение: '),int(input('Введите шаг: '))))
 print(decrypt(input('Введите сообщение: '),int(input('Введите шаг: '))))
+
+
